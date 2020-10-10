3fc90c94483451a439b2cc378cda855863855cc0
kmehdiza <kmehdiza@gmail.com>
2020-10-10 23:24:31 +0200

    Eduactive Array

--- src/com/algorithm/array/eduactive/Challenge3.java
+++ src/com/algorithm/array/eduactive/Challenge3.java
@@ -1,5 +1,8 @@
 package com.algorithm.array.eduactive;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Challenge3 {
 
     public static void main(String[] args) {
@@ -7,12 +10,27 @@ public class Challenge3 {
         int[] arr = {1, 21, 3, 14, 5, 60, 7, 6};
         int value = 27;
 
-        findSum(arr,value);
+        findSum(arr, value);
 
     }
 
-    public static void findSum(int[] arr, int n) {
+    public static int[] findSum(int[] arr, int n) {
+        List<Integer> list = new ArrayList<>();
+        //List<Integer> newList = new ArrayList<>();
+        int[] result = new int[2];
+        for (int num : arr) {
+            list.add(num);
+        }
 
+        for (int i = 0; i < arr.length-1; i++) {
+            int value = n - arr[i];;
+            if (list.contains(value)){
+                result[0]=(list.get(list.indexOf(value)));
+                result[1] = arr[i];
+            }
+        }
+        return result;
+        //list.stream().mapToInt(i->i).toArray();
     }
 
 }
--- src/com/algorithm/array/leetcode/XOROperationAnArray.java
+++ src/com/algorithm/array/leetcode/XOROperationAnArray.java
@@ -1,2 +1,24 @@
-package com.algorithm.array.leetcode;public class XOROperationAnArray {
+package com.algorithm.array.leetcode;
+
+public class XOROperationAnArray {
+
+    public static void main(String[] args) {
+        xorOperation(10,5);
+    }
+
+    public static int xorOperation(int n, int start) {
+        int[] nums = new int[n];
+
+        for (int i=0; i<n;i++){
+            nums[i] = start+2*i;
+        }
+
+        int xor = 0;
+        for (int num: nums){
+            xor^=num;
+        }
+
+        return xor;
+    }
+
 }
